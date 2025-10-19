986d448285fc2fb0ba80cbc67791f1cdca912d47
miguel <arrietaeguren@gmail.com>
2025-10-19 20:34:28 +0200

    try fix base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/vocaciones/', // Set the base to /vocaciones/
   title: 'Vocaciones',
   description: 'Vocaciones Navarra'
 })
