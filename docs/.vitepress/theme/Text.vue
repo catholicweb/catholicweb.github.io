b8cd531a76be846169ef76ffd5398c60355d9556
miguel <arrietaeguren@gmail.com>
2025-10-22 09:05:50 +0200

    tailwind typography

--- docs/.vitepress/theme/Text.vue
+++ docs/.vitepress/theme/Text.vue
@@ -12,7 +12,7 @@ defineProps({
   <section class="py-16">
     <div class="container mx-auto px-4">
       <h2 v-if="block.title" class="text-4xl font-bold text-center mb-4">{{ block.title }}</h2>
-      <div v-if="block.description" class="text-lg mb-12 max-w-2xl mx-auto" v-html="block.descriptionHtml"></div>
+      <div v-if="block.description" class="prose lg:prose-xl text-lg mb-12 max-w-2xl mx-auto" v-html="block.descriptionHtml"></div>
     </div>
   </section>
 </template>
