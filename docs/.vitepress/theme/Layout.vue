a04dc787ea8528b0d68c2226cfd001c10fb1e07d
miguel <arrietaeguren@gmail.com>
2025-10-22 16:26:17 +0200

    add content block

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -4,20 +4,20 @@
     <!-- Navbar Component -->
     <Navbar />
 
-
     <!--<Hero :block="page.frontmatter"/>-->
-
 
     <!-- Main Content - Block System -->
     <main class="flex-1">
-        <section v-for="(block, index) in page.frontmatter.blocks">
-            <component 
-              :key="index"
-              :is="getBlockComponent(block.type)"
-              :block="block"
-            />
-        </section>
+      <section v-for="(block, index) in page.frontmatter.blocks">
+        <component
+          :key="index"
+          :is="getBlockComponent(block.type)"
+          :block="block"
+        />
+      </section>
     </main>
+
+    <Content />
 
     <!-- Footer Component -->
     <Footer />
@@ -25,17 +25,17 @@
 </template>
 
 <script setup>
-import { useData } from 'vitepress'
-import Navbar from './Navbar.vue'
-import Footer from './Footer.vue'
-import Text from './Text.vue'
-import Hero from './Hero.vue'
-import Gallery from './Gallery.vue'
-import Accordion from './Accordion.vue'
-import Cta from './Cta.vue'
-import Carousel from './Carousel.vue'
+import { useData } from "vitepress";
+import Navbar from "./Navbar.vue";
+import Footer from "./Footer.vue";
+import Text from "./Text.vue";
+import Hero from "./Hero.vue";
+import Gallery from "./Gallery.vue";
+import Accordion from "./Accordion.vue";
+import Cta from "./Cta.vue";
+import Carousel from "./Carousel.vue";
 
-const { page } = useData()
+const { page } = useData();
 
 // Map block types to components
 const blockComponents = {
@@ -46,9 +46,9 @@ const blockComponents = {
   accordion: Accordion,
   cta: Cta,
   carousel: Carousel,
-}
+};
 
 const getBlockComponent = (type) => {
-  return blockComponents[type] || ContentBlock
-}
+  return blockComponents[type] || ContentBlock;
+};
 </script>
